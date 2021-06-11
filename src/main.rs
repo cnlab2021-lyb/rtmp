4ca422b6f8577fb6f2b8c8d21156dd843d9098da
TaWeiTu <tu.da.wei@gmail.com>
2021-06-12 01:24:10 +0800

    Make server port number configurable

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,12 @@ use error::{Error, Result};
 use server::RtmpServer;
 
 fn main() -> Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:7122").map_err(Error::Io)?;
+    let port = std::env::var("PORT")
+        .unwrap_or_else(|_| String::from("7122"))
+        .parse::<u16>()
+        .expect("Invalid port number");
+    let listener = TcpListener::bind(format!("127.0.0.1:{}", port)).map_err(Error::Io)?;
+    println!("Running RTMP server on port {}", port);
 
     for stream in listener.incoming() {
         thread::spawn(|| -> Result<()> {
