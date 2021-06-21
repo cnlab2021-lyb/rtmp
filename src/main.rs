95296334d64c8b3400306ef80db448aeaf55bd89
TaWeiTu <tu.da.wei@gmail.com>
2021-06-21 23:45:34 +0800

    Use tokio::net::{TcpListener, TcpStream}

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,36 @@
-use std::collections::HashMap;
-use std::net::TcpListener;
-use std::sync::{Arc, Mutex};
-use std::thread;
-
 mod amf;
 mod error;
 mod server;
 mod stream;
 mod utils;
 
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+use tokio::net::TcpListener;
+
 use error::{Error, Result};
 use server::{RtmpMediaStream, RtmpServer};
 
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
     let port = std::env::var("PORT")
         .unwrap_or_else(|_| String::from("7122"))
         .parse::<u16>()
         .expect("Invalid port number");
-    let listener = TcpListener::bind(format!("127.0.0.1:{}", port)).map_err(Error::Io)?;
+    let listener = TcpListener::bind(format!("127.0.0.1:{}", port)).await.map_err(Error::Io)?;
     println!("Running RTMP server on port {}", port);
 
     let media_streams = Arc::new(Mutex::new(HashMap::<String, RtmpMediaStream>::new()));
-
-    for stream in listener.incoming() {
+    loop {
+        let (stream, _) = listener.accept().await.map_err(Error::Io)?;
+        let stream = stream.into_std().map_err(Error::Io)?;
+        stream.set_nonblocking(false).map_err(Error::Io)?;
         let m = Arc::clone(&media_streams);
-        let stream = stream.map_err(Error::Io)?;
-        thread::spawn(move || {
+        tokio::spawn(async move {
             let mut server = RtmpServer::new(stream, m);
             if let Err(e) = server.serve() {
                 eprintln!("Error: {}", e);
             }
         });
     }
-    Ok(())
 }
